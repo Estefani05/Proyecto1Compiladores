41ca45d68ffe02511af356103e2e1320b33e7209
KeingellMoodie <80055929+KeingellMoodie@users.noreply.github.com>
2025-04-29 09:09:45 -0600

    Se agrego funcion para ver los tokens del lexer

--- Proy1Comp/app/src/main/java/org/example/MainFlexCup.java
+++ Proy1Comp/app/src/main/java/org/example/MainFlexCup.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 
+import org.example.ParserLexer.BasicLexerCup;
+
 import java_cup.internal_error;
 import java_cup.parser;
 import java_cup.runtime.Symbol;
@@ -23,9 +25,34 @@ public class MainFlexCup {
         jflex.Main.generate(strArr);
     }
 
+    public parser crearParser(BasicLexerCup lexer) throws Exception {
+        parser p = new parser(lexer);
+        return p;
+    }
+
     //Genera los archivos del parser
     public void Generateparser(String ruta) throws internal_error, IOException, Exception{
         String[] strArr = {ruta};
         java_cup.Main.main(strArr);
+    }
+
+    public void ejercicioLexer(String rutaScanear) throws IOException
+    {
+        Reader reader = new BufferedReader(new FileReader (rutaScanear));
+        reader.read();
+        BasicLexerCup lex = new BasicLexerCup(reader);
+        int i = 0;
+        Symbol token;
+        while(true){
+
+            token = lex.next_token();
+            if(token.sym != 0){
+                System.out.println("Token: " + token.sym + ", Valor: " + (token.value == null ? lex.yytext() : token.value.toString()));
+            }else{
+                System.out.println("Cantidad de lexemas encontrados: "+i);
+                return;
+            }
+            i++;
+        }
     }
 }
